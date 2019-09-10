aa2e56f03b1575e6a9d398cb71535fa41d6afdbd
SunshineFett <sunshineFett@gmail.com>
2019-09-10 06:14:27 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// Generate random color
+// // RGB 0-255
+// Apply to dom
+// update color on event
+
+const generateColorValue = () => Math.floor(Math.random() * 256);
+
+const createColor = () => {
+  const red = generateColorValue();
+  const green = generateColorValue();
+  const blue = generateColorValue();
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const applyColorToBody = color => {
+  return (document.body.style.backgroundColor = color);
+};
+
+const addRandomColorToBg = () => {
+  const color = createColor();
+  return applyColorToBody(color);
+};
+
+console.log(addRandomColorToBg());
